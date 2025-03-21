5e1d5d3a21abc43041feae5febcef6727b056d86
Vitor Costa <Vitorxd12.12@gmail.com>
2025-03-21 14:12:17 -0300

    navbar menu

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function toggleMenu() {
+    let menu = document.getElementById("mobile-menu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
+
